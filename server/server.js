b7d4a3e865a9781da60a8b60daf63788bef969c9
Ather Ahmad <atherahmad49@yahoo.com>
2023-10-26 10:04:38 +0200

    Port updated with houseno

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const __dirname = path.dirname(__filename)
 
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.HOUSENO || 4000;
 
 app.use(express.static(path.join(__dirname, '../client/dist/')))
 
